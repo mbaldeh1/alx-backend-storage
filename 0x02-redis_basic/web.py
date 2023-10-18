a1c90e7a434c49fa20cf1c4465c81db5f62daa23
mbaldeh1 <muhammadoubaldeh@gmail.com>
2023-10-18 19:49:38 +0000

    updated

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-"""
-Task: Working with Redis
-"""
+'''A module with tools for request caching and tracking.
+'''
 import redis
 import requests
 from functools import wraps
@@ -9,11 +8,17 @@ from typing import Callable
 
 
 redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
 
 
 def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
     @wraps(method)
     def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
         redis_store.incr(f'count:{url}')
         result = redis_store.get(f'result:{url}')
         if result:
@@ -27,4 +32,7 @@ def data_cacher(method: Callable) -> Callable:
 
 @data_cacher
 def get_page(url: str) -> str:
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
     return requests.get(url).text
